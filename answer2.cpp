195dbb3be0496764ee1956f9a533c6b3f9533ea5
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-08-25 19:44:30 +0530

    Create answer2.cpp

---
+++ answer2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float c,f;
+	cout << "Enter Temperature in Celsius:" << endl;
+	cin >> c;
+	f=c*1.8+32;
+	cout << "Temeperature in Fahrenheit=" << f << "F";
+	
+	
+
+	return 0;
+}
