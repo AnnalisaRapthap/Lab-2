58a1199cd48a351f82747411722e014115a54d55
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-08-25 20:16:16 +0530

    Create answer6.cpp

---
+++ answer6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	float a,s;
+	cout << "Enter a number:";
+	cin >> a;
+	s=sqrt(a);
+	cout << "Square root of the number=" << s;
+	return 0;
+}
